537c5a27d3cb391e3073989a34530c33156c4e99
Smit <smitshah95@hotmail.com>
2020-02-12 01:01:34 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# jsonschema2pojo-test
+Testing application for jsonschema2pojo.
+
+This is a small application that demonstrates use of jsonschema2pojo to generate a POJO from a JSON schema.
+
+# Build
+
+    mvn clean package
+
+# Test
+
+    # Parse and print name from http://store.steampowered.com/api/appdetails?appids=10
+    java -jar target/test-json.jar 10
+    Counter-Strike
+
+    # Parse and print name from http://store.steampowered.com/api/appdetails?appids=219
+    java -jar target/test-json.jar 219
+    Half-Life 2: Demo
---
+++ src/main/java/View.java
@@ -0,0 +1,25 @@
+import java.net.URI;
+import java.util.Map;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import model.Game;
+import model.Value;
+
+public class View {
+
+    public static void main(String[] args) throws Exception {
+
+        URI uri = new URI("http://api.icndb.com/jokes/random");
+        ObjectMapper objectMapper = new ObjectMapper();
+        Object obj = objectMapper.readValue(uri.toURL(), Object.class);
+        Game game = objectMapper.convertValue(obj, Game.class);
+        game.setAmount(new Double("80.0"));
+        System.out.println(game.getAmount());
+        Value v = game.getValue();
+        System.out.println(v.getId());
+        System.out.println(v.getJoke());
+        System.out.println(v.getCategories());
+    }
+}
